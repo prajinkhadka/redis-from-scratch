447a81969346780b540519ba4057462ca988066a
prajinkhadka <prawjeenkhadka@gmail.com>
2024-02-11 13:22:54 -0600

    Handling ECHO

--- app/main.py
+++ app/main.py
@@ -20,7 +20,7 @@ async def handle_client(reader, writer):
         print("The request for echo is", request)
 
         if request[2] == "echo":
-            response_value  = request[4] + "\r\n"
+            response_value  =  "+" + request[4] + "\r\n"
             print("The response_value is", response_value)
             await writer.write(response_value.encode())
 
