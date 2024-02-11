d12ab18ac651f5e9d03a918279071d9e3194788f
prajinkhadka <prawjeenkhadka@gmail.com>
2024-02-11 12:19:07 -0600

    Data recieved

--- app/main.py
+++ app/main.py
@@ -18,17 +18,18 @@ def handle_connection(server_socket):
 
     client_connection, client_address = server_socket.accept()
     with client_connection:
-        print(f"Now connected to {client_address}")
-        data = client_connection.recv(1024).decode() 
-        print("The data recieved is", data)
+        while True:
+            print(f"Now connected to {client_address}") 
+            data = client_connection.recv(1024).decode() 
+            print("The data recieved is", data)
 
-        # the data recieved for ping will be something like this : *1\r\n$4\r\nping\r\n 
-        print("The raw data is", data)
-        request = data.split("\r\n")
-        print("The request recieved is", request)
+            print("The raw data is", data)
+            # the data recieved for ping will be something like this : *1\r\n$4\r\nping\r\n 
+            request = data.split("\r\n")
+            print("The request recieved is", request)
 
-        if 'ping' in request:
-            client_connection.sendall(PING_RESPONSE.encode())
+            if 'ping' in request:
+                client_connection.sendall(PING_RESPONSE.encode())
 
 if __name__ == "__main__":
     main()
