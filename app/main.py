16b9caaf356a9b22b5ca1ac9fcdfc86fa95a5aad
prajinkhadka <prawjeenkhadka@gmail.com>
2024-02-11 10:44:59 -0600

    Create a tcp socket

--- app/main.py
+++ app/main.py
@@ -1,10 +1,14 @@
 # Uncomment this to pass the first stage
 # import socket
+import socket
 
 
 def main():
     # You can use print statements as follows for debugging, they'll be visible when running tests.
     print("Logs from your program will appear here!")
+
+    server_socket = socket.create_server(("localhost", 6379), reuse_port = True)
+    server_socket.accept()
 
     # Uncomment this to pass the first stage
     #
